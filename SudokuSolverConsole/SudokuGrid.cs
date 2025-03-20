cfc2a50cbfac153c5ecf4d1e76b32b37e42c5c26
jako1o <jakob@stoye.net>
2025-03-20 18:03:12 +0100

    Projekt erstellt

---
+++ SudokuSolverConsole/SudokuGrid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolverConsole
+{
+    internal class SudokuGrid
+    {
+
+        //Attribute
+        protected uint[][] grid;
+        //Konstruktoren
+
+        //Methoden
+
+        protected uint[][] CreateGrid(uint[][] gr)
+        {
+            //Überprüfen, ob eingegebenes Array valid ist.
+
+
+            //Array Dimensionen
+            bool LenghtValid;
+
+            if (gr.GetLength(0) == 9 && gr.GetLength(1) == 9)
+            {
+                LenghtValid = true;
+            }
+            else
+            {
+                LenghtValid = false;
+            }
+
+            //Zahlen zwischen 1 und 9
+            bool NumbersValid;
+
+        }
+
+    }
+}
